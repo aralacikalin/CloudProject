327085223453271e3023515bf50f8e45315c7541
Aral Acikalin <aralacikalin@hotmail.com>
2020-10-23 20:11:45 +0300

    items can be downloaded from client

--- client/src/components/CloudItem/CloudItem.jsx
+++ client/src/components/CloudItem/CloudItem.jsx
@@ -1,17 +1,48 @@
 import React, { Component } from 'react';
+import Link from '@material-ui/core/Link';
+import axios from "axios"
 
 class CloudItem extends Component {
   constructor(props){
     super(props)
-
+    this.preventDefault=this.preventDefault.bind(this)
   }
+  preventDefault(event){
+    /*
+    //!not very safe
+    fetch(`/download/${this.props.item}`)
+      .then(res=>{
+        if(res.ok){
+          window.open(`http://localhost:4000/download/${this.props.item}`)
+        }
+      });
+      */
+     //probably safer alternative than above
+     fetch(`/download/${this.props.item}`).then(response => {
+      const type = response.headers['content-type'];
+      const filename = response.headers
+          .get("content-disposition")
+          .split('"')[1];
+      const blob = new Blob([response.data], { type: type, encoding: 'UTF-8' });
+      //const url = window.URL.createObjectURL(blob);
+      const link = document.createElement('a');
+      link.href = window.URL.createObjectURL(blob);
+      link.download = filename; // Here I want to get rid of hardcoded value instead I want filename from server
+      link.click();
+      link.remove(); //  Probably needed to remove html element after downloading?
+    });
 
+    event.preventDefault()
+  }
+  
 
   render(){
 
     return (
       <div>
-          SINGLE CLOUD ITEM
+          <Link href={`/download/${this.props.item}`} onClick={this.preventDefault}>
+            {this.props.item}
+          </Link>
       </div>
     );
   }
--- client/src/containers/CloudItemsMenu/CloudItemsMenu.jsx
+++ client/src/containers/CloudItemsMenu/CloudItemsMenu.jsx
@@ -2,23 +2,48 @@ import React, { Component } from 'react';
 import {CloudItem} from '../../components';
 
 class CloudItemsMenu extends Component {
-  constructor(props){
-    super(props)
+    ismounted=false
+    constructor(props){
+        super(props)
 
-  }
+        this.state={
+            items:[]
+        }
+        this.fetchAll=this.fetchAll.bind(this)
+    }
+
+    async componentDidMount(){
+        this.fetchAll();
+        
+    }
+    componentWillUnmount(){
+        this.isUnmounted=true;
+    }
+    async fetchAll(){
+        const res = await fetch("/content")
+        const data =await res.json()
+        console.log(data)
+        if(this.isUnmounted){
+            return;
+        }
+        else{
+
+            this.setState({items:data})
+            console.log(this.state.items)
+        }
+
+    }
 
 
-  render(){
+    render(){
 
-    return (
-      <div>
-          CLOUD ITEMS
-          <CloudItem></CloudItem>
-          <CloudItem></CloudItem>
-          <CloudItem></CloudItem>
-      </div>
-    );
-  }
+        return (
+            <div>
+                CLOUD ITEMS
+                {this.state.items.map((val,i)=>(<CloudItem key={i} item={val} />))}
+            </div>
+        );
+    }
 }
 
 export default CloudItemsMenu;
