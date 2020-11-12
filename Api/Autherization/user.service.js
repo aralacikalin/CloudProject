ef1f281db51c80af74d1da49c3b1243fe4dc4840
Aral Acikalin <aralacikalin@hotmail.com>
2020-11-12 15:02:28 +0300

    user json created

--- Api/Autherization/user.service.js
+++ Api/Autherization/user.service.js
@@ -1,12 +1,15 @@
 ﻿const config = require('../config.json');
 const jwt = require('jsonwebtoken');
 const Role = require('../_helpers/role');
+const {users} =require("./users.json")
 
 // users hardcoded for simplicity, store in a db for production applications
+/*
 const users = [
     { id: 1, username: 'admin', password: 'admin', firstName: 'Admin', lastName: 'User', role: Role.Admin },
     { id: 2, username: 'user', password: 'user', firstName: 'Normal', lastName: 'User', role: Role.User }
 ];
+*/
 
 module.exports = {
     authenticate,
