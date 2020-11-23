2e80904a73b457ec583e18cd860908ad159624e7
Aral Acikalin <aralacikalin@hotmail.com>
2020-11-23 17:33:43 +0300

    downloaded file is store in the folder CloudContents

--- Api/WebScraperScripts/LinkDownloader.py
+++ Api/WebScraperScripts/LinkDownloader.py
@@ -6,10 +6,16 @@ import sys
 
 options = webdriver.ChromeOptions() 
 options.add_argument("download.default_directory="+os.path.abspath(os.getcwd())+r"\SeleniumWebDriver")
+options.add_experimental_option("prefs", {
+  "download.default_directory": os.getcwd()+r"\CloudContents",
+  "download.prompt_for_download": False,
+  "download.directory_upgrade": True,
+  "safebrowsing.enabled": True
+})
 
 PATH= os.path.abspath(os.getcwd())+r"\WebScraperScripts\SeleniumWebDriver\chromedriver"
 
-#TODO automaticly click keep to downloading file
+#TODO automaticly close chrome after downloading.
 
 driver=webdriver.Chrome(executable_path= PATH, options=options)
 driver.get(sys.argv[1])
