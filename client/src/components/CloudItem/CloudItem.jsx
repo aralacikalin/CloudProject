c2cc6d00c45e6949e03aadee79a1c79504fd57eb
Aral Acikalin <aralacikalin@hotmail.com>
2020-11-26 15:16:54 +0300

    cloud item component and cloud item container have a good design now

--- client/src/components/CloudItem/CloudItem.jsx
+++ client/src/components/CloudItem/CloudItem.jsx
@@ -1,12 +1,53 @@
 import React, { Component } from 'react';
-import Link from '@material-ui/core/Link';
 import axios from "axios"
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
 
 class CloudItem extends Component {
   constructor(props){
     super(props)
     this.preventDefault=this.preventDefault.bind(this)
+    this.classes=this.classes.bind(this)
+    this.useStyles=this.useStyles.bind(this)
   }
+  useStyles = ()=> makeStyles((theme) => ({
+    icon: {
+      marginRight: theme.spacing(2),
+    },
+    heroContent: {
+      backgroundColor: theme.palette.background.paper,
+      padding: theme.spacing(8, 0, 6),
+    },
+    heroButtons: {
+      marginTop: theme.spacing(4),
+    },
+    cardGrid: {
+      paddingTop: theme.spacing(8),
+      paddingBottom: theme.spacing(8),
+    },
+    card: {
+      height: '100%',
+      display: 'flex',
+      flexDirection: 'column',
+    },
+    cardMedia: {
+      paddingTop: '56.25%', // 16:9
+    },
+    cardContent: {
+      flexGrow: 1,
+    },
+    footer: {
+      backgroundColor: theme.palette.background.paper,
+      padding: theme.spacing(6),
+    },
+  }));
+  classes =()=> this.useStyles();
   preventDefault(event){
     /*
     //!not very safe
@@ -41,6 +82,37 @@ class CloudItem extends Component {
 
   render(){
 
+
+    return(
+      
+      <Card className={this.classes.card}>
+        <CardMedia
+          className={this.classes.cardMedia}
+          component="img"
+          src={`http://localhost:4000/cloudcontents/${this.props.item}`}
+          title={this.props.item}
+        />
+        <CardContent className={this.classes.cardContent}>
+          <Typography gutterBottom variant="h5" component="h2">
+            {this.props.item}
+          </Typography>
+          <Typography>
+            Insert Possible File Information (like size, type)
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small" color="primary" onClick={this.preventDefault}>
+            Download
+          </Button>
+          <Button size="small" color="primary">
+            Edit
+          </Button>
+        </CardActions>
+      </Card>
+    );
+
+
+    /*
     return (
       <div>
           <Link onClick={this.preventDefault}>
@@ -48,6 +120,7 @@ class CloudItem extends Component {
           </Link>
       </div>
     );
+    */
   }
 }
 
