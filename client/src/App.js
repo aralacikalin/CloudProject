b7ec3a0891126a314e5fe5510600edd904a22b7e
Aral Acikalin <aralacikalin@hotmail.com>
2020-11-27 13:08:05 +0300

    changed to dark theme

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,6 @@
+
+import { createMuiTheme,  ThemeProvider } from '@material-ui/core/styles';
+import { indigo,cyan } from '@material-ui/core/colors';
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import {Main,CloudItemsMenu,LoginScreen,Nav} from "./containers"
@@ -5,15 +8,25 @@ import {Main,CloudItemsMenu,LoginScreen,Nav} from "./containers"
 
 class App extends Component {
 
+  theme = createMuiTheme({
+    palette: {
+      type:"dark",
+      primary: {main:"#90caf9"},
+      
+    }
+  });
+
   render(){
 
     return (
       <Router>
-        <Nav/>
-        <Route exact path="/LoginScreen" component={LoginScreen} />
-        <Route exact path="/" component={Main} />
-        <Route exact path="/clouditems" component={CloudItemsMenu} />
-      </Router>
+        <ThemeProvider theme={this.theme}>
+          <Nav/>
+          <Route exact path="/LoginScreen" component={LoginScreen} />
+          <Route exact path="/" component={Main} />
+          <Route exact path="/clouditems" component={CloudItemsMenu} />
+      </ThemeProvider>
+        </Router>
     );
   }
 }
