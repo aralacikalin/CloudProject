c5b1221332bdf564ba9836603933678d6abea4c6
Aral Acikalin <aralacikalin@hotmail.com>
2020-12-09 14:00:39 +0300

    small changes

--- client/src/components/FileInput/FileInput.jsx
+++ client/src/components/FileInput/FileInput.jsx
@@ -5,6 +5,8 @@ import Button from '@material-ui/core/Button';
 import Snackbar from '@material-ui/core/Snackbar';
 import MuiAlert from '@material-ui/lab/Alert';
 import CloudUploadIcon from '@material-ui/icons/CloudUpload'; //TODO can use this icon
+import Grid from '@material-ui/core/Grid';
+
 
 
 
@@ -52,27 +54,33 @@ class FileInput extends Component {
 
     return (
         <div>
+            <Grid container spacing={2} justify="center">
+                <Grid item>
+                    <Dropzone onDrop={acceptedFiles => {this.setState({files:acceptedFiles})}}>
+                        {({getRootProps, getInputProps}) => (
+                        <section>
+                            <div {...getRootProps()}>
+                            <input multiple {...getInputProps()} />
+                            <p>Drag 'n' drop some files here, or click to select files</p>
+                            <h4>Files</h4>
+                            <ul>{this.state.files.map(file=>( <li key={file.name}>
+                            {file.name} - {(parseInt(file.size)/1000000).toFixed(1)} Megabytes
+                        </li>))}</ul>
+                            </div>
+                        </section>
+                        )}
+                    </Dropzone>
+                </Grid>
+                <Grid item>
+                    <div>{this.state.uploadProgress}</div>
+                    <Button variant="outlined" color="primary" onClick={this.handleUpload}>Submit Files</Button>
+                </Grid>
+            </Grid>
             <Snackbar open={this.state.isUploadSnack} autoHideDuration={6000} onClose={this.handleSnackClose}  anchorOrigin={{vertical: 'bottom',horizontal: 'left',}}>
                       <MuiAlert onClose={this.handleSnackClose} severity="success" elevation={6} variant="standard" color="success">
                         Successfully Uploaded Files!
                       </MuiAlert>
                     </Snackbar>
-            <Dropzone onDrop={acceptedFiles => {this.setState({files:acceptedFiles})}}>
-                {({getRootProps, getInputProps}) => (
-                <section>
-                    <div {...getRootProps()}>
-                    <input multiple {...getInputProps()} />
-                    <p>Drag 'n' drop some files here, or click to select files</p>
-                    <h4>Files</h4>
-                    <ul>{this.state.files.map(file=>( <li key={file.name}>
-                    {file.name} - {file.size} bytes
-                </li>))}</ul>
-                    </div>
-                </section>
-                )}
-            </Dropzone>
-            <div>{this.state.uploadProgress}</div>
-            <Button variant="outlined" color="primary" onClick={this.handleUpload}>action</Button>
         </div>
       
     );
