5be0869aa2eb7930eded248f802612ad781d1848
Aral Acikalin <aralacikalin@hotmail.com>
2020-12-28 13:50:35 +0300

    now folders inside cloud contents changed to zip and can be downloaded as zip and a unauterized request bug is fixed with clouditem component

--- Api/routes/content.js
+++ Api/routes/content.js
@@ -4,6 +4,10 @@ var fs =require("fs")
 var authorize =require("../_helpers/authorize");
 var Role =require("../_helpers/role");
 var path=require("path")
+var {exec,execSync} =require("child_process");
+const zipFolder = require('folder-zip-sync')
+ 
+
 
 /* item full name in the url is needed ex: download/myFile.txt */
 router.get('/', authorize(Role.Admin) ,function(req, res, next) { 
@@ -15,7 +19,15 @@ router.get('/', authorize(Role.Admin) ,function(req, res, next) {
     files.forEach((val,i)=>{
         var size=fs.statSync("./CloudContents/"+val).size
         var ext=path.extname("./CloudContents/"+val)
-        fileArray.push([val,(size/(1024*1024)),ext.split(".")[1]])
+        if(ext.split(".")[1]!=undefined){
+
+            fileArray.push([val,(size/(1024*1024)),ext.split(".")[1]])
+        }
+        else{
+            var zipName=val+'.zip'
+            zipFolder('./CloudContents/'+val,"./CloudContents/"+zipName )
+
+        }
     })
     res.send(fileArray)
 
