a25a4a5c3225d26198098798197e89ad1b559efe
Aral Acikalin <aralacikalin@hotmail.com>
2021-01-07 12:06:01 +0300

    list view is created but not fully tested and design could be improven

--- client/src/components/CloudItems/CloudItems.jsx
+++ client/src/components/CloudItems/CloudItems.jsx
@@ -7,8 +7,9 @@ import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
 import {CloudItem} from '../../components';
 
-import { TextField } from '@material-ui/core';
+import { Button, TextField } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
+import ListIcon from '@material-ui/icons/List';
 
 
 import Snackbar from '@material-ui/core/Snackbar';
@@ -21,7 +22,8 @@ class CloudItems extends Component {
 
         this.state={
             search:"",
-            isDeleteAlert:false
+            isDeleteAlert:false,
+            isListView:false
         }
 
     }
@@ -33,6 +35,9 @@ class CloudItems extends Component {
     isDeleted=()=>{
         this.setState({isDeleteAlert:true})
     }
+    handleIsList=()=>{
+        this.setState({isListView:!this.state.isListView})
+    }
 
 
     render(){
@@ -40,7 +45,7 @@ class CloudItems extends Component {
         return (
             <React.Fragment>
                 <CssBaseline />
-                <Container maxWidth="xl">
+                <Container maxWidth={this.state.isListView?("md"):("xl")}>
                     
                     {/* End hero unit */}
                     <Container maxWidth="sm" >
@@ -49,17 +54,24 @@ class CloudItems extends Component {
                             <Grid item xs={1} >
                                 <SearchIcon />
                             </Grid>
-                            <Grid item xs={11}>
+                            <Grid item xs={10}>
                                 <TextField id="standard-basic" fullWidth onChange={(event)=>{this.setState({search:event.target.value})}} label="Search"/>
+                            </Grid>
+                            <Grid item xs={1} >
+                                <Button onClick={this.handleIsList}>
+                                    <ListIcon />
+                                </Button>
                             </Grid>
                         </Grid>
                     </Container>
                     <Grid container spacing={4}>
                         {this.props.items&& this.props.items.filter(item=>item[0].toLowerCase().includes(this.state.search.toLowerCase().trim())||item[2].includes(this.state.search.trim())).map((item) => (
-                        <Grid item key={item[0]} xs={12} sm={6} md={4} lg={3}> 
-                            <CloudItem refresh={this.props.refresh} isDeleted={this.isDeleted} item={item[0]} size={item[1]} ext={item[2]} ip={"http://"+this.props.ip+":4000/"}/>
-                            
-                        </Grid>
+                        !this.state.isListView?(<Grid item key={item[0]} xs={12} sm={6} md={4} lg={3}> 
+                            <CloudItem isListView={false} refresh={this.props.refresh} isDeleted={this.isDeleted} item={item[0]} size={item[1]} ext={item[2]} ip={"http://"+this.props.ip+":4000/"}/>
+                        </Grid>):
+                        (<Grid item key={item[0]} xs={12}> 
+                            <CloudItem isListView={true} refresh={this.props.refresh} isDeleted={this.isDeleted} item={item[0]} size={item[1]} ext={item[2]} ip={"http://"+this.props.ip+":4000/"}/>
+                        </Grid>)
                         ))}
                     </Grid>
                     </Container>
